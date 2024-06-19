ca0e5e3f508114d1ab45f31671a45f6b23c6edf7
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-06-19 19:53:23 +0200

    ContextInterface extends NullableInterface

--- src/Contracts/Context/ContextInterface.php
+++ src/Contracts/Context/ContextInterface.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace Atournayre\Contracts\Context;
 
 use Atournayre\Contracts\DateTime\DateTimeInterface;
+use Atournayre\Contracts\Null\NullableInterface;
 use Atournayre\Contracts\Security\UserInterface;
 
-interface ContextInterface
+interface ContextInterface extends NullableInterface
 {
     public function user(): UserInterface;
 
