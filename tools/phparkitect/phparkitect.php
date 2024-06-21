138eb4aec77e5a9bc93fbc0b861d16805f2ec60f
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-06-22 00:37:52 +0200

    Remove atournayre/phparkitect-rules

--- tools/phparkitect/phparkitect.php
+++ tools/phparkitect/phparkitect.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 use Arkitect\ClassSet;
 use Arkitect\CLI\Config;
-use Atournayre\PHPArkitect\Builder\RuleBuilder;
 
 return static function (Config $config): void {
     $classSet = ClassSet::fromDir(__DIR__.'/../../src');
 
-    $rules = RuleBuilder::create()
-        ->rules();
+    $rules = [];
 
     $config->add($classSet, ...$rules);
 };
