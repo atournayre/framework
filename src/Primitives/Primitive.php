168fb70dac85f026c50765eedd775c162c21aa73
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-07-01 15:42:22 +0200

    PHP CS Fixer

--- src/Primitives/Primitive.php
+++ src/Primitives/Primitive.php
@@ -7,11 +7,18 @@ namespace Atournayre\Primitives;
 final class Primitive
 {
     public const STRING = 'string';
+
     public const INT = 'int';
+
     public const FLOAT = 'float';
+
     public const BOOL = 'bool';
+
     public const ARRAY = 'array';
+
     public const OBJECT = 'object';
+
     public const NULL = 'null';
+
     public const MIXED = 'mixed';
 }
