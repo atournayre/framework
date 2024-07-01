fdb0263c5cac447ca9e20856a785dc4c13dea140
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-07-01 15:40:35 +0200

    Create Primitive to list all ths primitives types

---
+++ src/Primitives/Primitive.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Atournayre\Primitives;
+
+final class Primitive
+{
+    public const STRING = 'string';
+    public const INT = 'int';
+    public const FLOAT = 'float';
+    public const BOOL = 'bool';
+    public const ARRAY = 'array';
+    public const OBJECT = 'object';
+    public const NULL = 'null';
+    public const MIXED = 'mixed';
+}
