ce3b3da349025b3850475c947c42ea86dc99ffa7
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2025-04-01 15:09:03 +0200

    feat(PHPStan): add AssertTypeSpecifyingExtension for type specification

---
+++ src/PHPStan/Extension/AssertTypeSpecifyingExtension.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Atournayre\PHPStan\Extension;
+
+use Atournayre\Common\Assert\Assert;
+
+final class AssertTypeSpecifyingExtension extends \PHPStan\Type\WebMozartAssert\AssertTypeSpecifyingExtension
+{
+    public function getClass(): string
+    {
+        return Assert::class;
+    }
+}
