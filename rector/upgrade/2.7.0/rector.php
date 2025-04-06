e2cb0d58ca243aeecb4e2b07c76145cb76cc01f5
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2025-04-06 19:22:30 +0200

    ✨ feat: update rector configuration to use current working directory for source path

--- rector/upgrade/2.7.0/rector.php
+++ rector/upgrade/2.7.0/rector.php
@@ -8,7 +8,7 @@ use Rector\Renaming\ValueObject\MethodCallRename;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/../../../src',
+        '%currentWorkingDirectory%/src',
     ])
     ->withConfiguredRule(RenameMethodRector::class, [
         new MethodCallRename(
