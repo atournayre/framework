5c583c16a1dd71d7590cdd6149cb2c5da0efae22
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2025-04-06 17:43:32 +0200

    ✨ feat: add rector configuration for method renaming in SplFileInfo

---
+++ rector/upgrade/2.7.0/rector.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
+
+return RectorConfig::configure()
+    ->withPaths([
+        '/src',
+    ])
+    ->withConfiguredRule(RenameMethodRector::class, [
+        new MethodCallRename(
+            'Atournayre\Wrapper\SplFileInfo',
+            'getRelativePath',
+            'relativePath'
+        ),
+        new MethodCallRename(
+            'Atournayre\Wrapper\SplFileInfo',
+            'getRelativePathname',
+            'relativePathname'
+        ),
+        new MethodCallRename(
+            'Atournayre\Wrapper\SplFileInfo',
+            'getFilenameWithoutExtension',
+            'filenameWithoutExtension'
+        ),
+        new MethodCallRename(
+            'Atournayre\Wrapper\SplFileInfo',
+            'getContents',
+            'contents'
+        ),
+        new MethodCallRename(
+            'Atournayre\Wrapper\SplFileInfo',
+            'getExtension',
+            'extension'
+        ),
+        new MethodCallRename(
+            'Atournayre\Wrapper\SplFileInfo',
+            'getSize',
+            'size'
+        ),
+        new MethodCallRename(
+            'Atournayre\Wrapper\SplFileInfo',
+            'getFilename',
+            'filename'
+        ),
+        new MethodCallRename(
+            'Atournayre\Wrapper\SplFileInfo',
+            'getPathname',
+            'pathname'
+        ),
+    ])
+;
