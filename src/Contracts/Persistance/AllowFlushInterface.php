ffe863de7ece2ea41614475ff1766a49087ab6d9
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2025-06-09 19:37:06 +0200

    ✨ feat: introduce `AllowFlushInterface` for automatic transaction management

---
+++ src/Contracts/Persistance/AllowFlushInterface.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Atournayre\Contracts\Persistance;
+
+/**
+ * Interface to mark classes that allow automatic transaction management.
+ *
+ * Classes implementing this interface will have transactions automatically
+ * started, committed, and rolled back:
+ * - Controllers via DoctrineTransactionSubscriber
+ * - Console commands via DoctrineCommandTransactionSubscriber
+ * - Messenger handlers via DoctrineTransactionMiddleware
+ */
+interface AllowFlushInterface
+{
+}
